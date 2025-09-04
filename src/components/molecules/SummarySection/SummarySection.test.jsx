135959ca808344db5d10d04e7bccd80d99499081
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-09-04 12:26:42 -0500

    fix: Incorrectly imported components

--- src/components/molecules/SummarySection/SummarySection.test.jsx
+++ src/components/molecules/SummarySection/SummarySection.test.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
-import { SummarySection } from "./SummarySection.jsx";
+import SummarySection from "./SummarySection.jsx";
 
 describe("SummarySection", () => {
     it("renders content correctly", () => {
